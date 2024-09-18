a0d69cb8c5ab37199b80e2d485c534b718b430b3
tawseefnabi <tawseefnabi9@gmail.com>
2024-09-18 11:22:48 +0530

    adding multiple ToggleSwitch components on the same page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
 
-function App() {
+import NewsletterStates from "./containers/NewsletterStates";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <NewsletterStates />
     </div>
   );
 }
-
-export default App;
